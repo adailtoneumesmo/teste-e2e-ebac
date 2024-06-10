8ae3e10834acb46f91a0069a35f8b764c50e758e
Adailton Alves da Silva <adailton.silva@cardway.com.br>
2024-06-10 14:49:44 -0300

    teste de push

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,56 @@
 /// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    beforeEach(() => {
+        cy.visit('minha-conta')
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    });
+
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+        cy.fixture('perfil').then((dados) => {
+            cy.login(dados.usuario, dados.senha, { log: false })
+        })
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get('.product-block ').eq(0).click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get('.product-block ').eq(2).click()
+        cy.get('.button-variable-item-36').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get('.product-block ').eq(4).click()
+        cy.get('.button-variable-item-36').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.get('.product-block ').eq(6).click()
+        cy.get('.button-variable-item-L').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        cy.get('#billing_address_1').clear().type(faker.location.streetAddress())
+        cy.get('#billing_city').clear().type(faker.location.city())
+        cy.get('#billing_postcode').clear().type(faker.location.zipCode('04965070'))
+        cy.get('#billing_phone').clear().type('11965843429')
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        cy.get('.page-title').should('exist')
+
+    });
 
 
 })
